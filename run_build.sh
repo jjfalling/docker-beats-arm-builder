2fe0e6f2b9dc523d65739605f6907f2afd0fcf06
Jeremy Falling <jjfalling@users.noreply.github.com>
2022-05-28 13:55:34 +0200

    Update run_build.sh

--- run_build.sh
+++ run_build.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+BUILD_ARCHS=("arm64" "arm")
+
 echo "Cloning beats repo"
-go get github.com/elastic/beats       
-cd /go/src/github.com/elastic/beats/
+# clone as go get behavior changed
+git clone https://github.com/elastic/beats ${GOPATH}/src/github.com/elastic/beats
 echo "Checking out version ${VERSION}"
+cd /go/src/github.com/elastic/beats/
 git checkout -q v${VERSION}
-for BEAT in $(ls -d *beat | egrep -v '(winlogbeat|libbeat)') 
+
+for ARCH in "${BUILD_ARCHS[@]}"
         do
-        cd /go/src/github.com/elastic/beats/$BEAT
-	echo "Building $BEAT"
-        GOARCH=arm go build 
-        [ -f $BEAT ] && cp -v $BEAT /build/$BEAT-${VERSION}
+        for BEAT in $(ls -d *beat | egrep -v '(winlogbeat|libbeat|packetbeat)')
+                do
+                cd /go/src/github.com/elastic/beats/$BEAT
+        echo "Building $BEAT for $ARCH"
+                GOARCH=$ARCH go build
+                [ -f $BEAT ] && cp -v $BEAT /build/$BEAT-${VERSION}-$ARCH
+        done
 done
+
 exit
